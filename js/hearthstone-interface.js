8db4792e47f23d64cd00147d7f9a40cb64397c47
Liam Stabeno and Riley Watts <liam@>
2017-04-19 14:35:04 -0700

    integrate memory game functionality

--- js/hearthstone-interface.js
+++ js/hearthstone-interface.js
@@ -1,12 +1,58 @@
 var hearthstoneCard = require('./../js/hearthstone.js').hearthstoneModule;
 
+var insertImg = function(src) {
+  return "<img src='"+src+"'/>";
+};
+
 
 $(function() {
-  console.log("things are looking up");
+  var hiddenImg = "http://media-hearth.cursecdn.com/attachments/39/684/cardback_20.png";
   var hearthCard = new hearthstoneCard();
-  console.log(hearthCard);
-  $("#here").click(function() {
-    hearthCard.getImg();
-
+  var cards = hearthCard.getImg();
+  console.log(cards);
+  console.log("things are looking up");
+  // console.log(hearthCard);
+  $("#there").click(function() {
+    event.preventDefault();
+    console.log("clicked");
   });
+
+  var correctGuesses = 0;
+  var clicks = [];
+  var reveal = function(index) {
+    if (clicks.length === 0) {
+      clicks.push(index);
+      $("#card"+(index)).html((cards[index]));
+    } else if (clicks.length === 1) {
+      clicks.push(index);
+    } if (cards[clicks[0]] === cards[index]) {
+        $("#card"+(index)).html((cards[index]));
+        $("#card"+(index)).off("click");
+        $("#card"+(clicks[0])).off("click");
+        correctGuesses += 2;
+        if (correctGuesses === cards.length) {
+          var won = setTimeout(function() {
+            alert("you WOn");
+          }, 100);
+        }
+        clicks = [];
+      } else {
+        $("#card"+(index)).html((cards[index]));
+        var reset = setTimeout(function() {
+          $("#card"+(index)).html((hiddenImg));
+          $("#card"+(clicks[0])).html((hiddenImg));
+          clicks = [];
+        }, 1000);
+      }
+    }
+
+for (var i = 0; i < 10; i++) {
+  $("#card"+i).html(insertImg(hiddenImg));
+  $("#card"+i).attr("data-index", i);
+  $("#card"+i).click(function() {
+    reveal($(this).attr("data-index"));
+  });
+  }
+
+
 });
--- js/hearthstone.js
+++ js/hearthstone.js
@@ -4,17 +4,38 @@ function hearthstoneCard() {
 
 }
 
-hearthstoneCard.prototype.getImg = function() {
-  console.log("Im inside the prototype!");
+hearthstoneCard.prototype.getCards = function(size) {
 
-  unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/Leeroy%20Jenkins")
+  return shuffleArray(cards);
+};
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+
+hearthstoneCard.prototype.getImg = function() {
+  // var number = (Math.floor(Math.random() * 192)+1);
+  var cards = [];
+  unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/qualities/Epic")
 .header("X-Mashape-Key", "rWHfwQgzLMmshxTdrypY3GZNpimop1Wg51mjsnFotWj6HoCmAN")
 .header("Accept", "application/json")
 .end(function (result) {
-  console.log(result.name);
-  console.log(result.body[0].artist);
-  // $("#solution").text(result.body.object.imgGold);
+  // console.log(result.body[number], result.body[number].name);
+  // console.log(result);
+  for (var i = 1; i <= 5; i++) {
+    var number = (Math.floor(Math.random() * 192)+1);
+    cards.push("<img src='"+result.body[number].imgGold+"'>");
+    cards.push("<img src='"+result.body[number].imgGold+"'>");
+  }
 });
+  return cards;
+
 };
 
 exports.hearthstoneModule = hearthstoneCard;
