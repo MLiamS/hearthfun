008a247f90be625a4d5c28d14dbe231af5455c54
liam stabeno and riley watts <riley@>
2017-04-20 09:08:52 -0700

    fix bug

--- js/hearthstone-interface.js
+++ js/hearthstone-interface.js
@@ -4,18 +4,6 @@ var insertImg = function(src) {
   return "<img src='"+src+"'/>";
 };
 
-function shuffleArray(array) {
-  console.log(array.length);
-    for(var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        console.log("im inside shuffle function");
-        array[i] = array[j];
-        array[j] = temp;
-    }
-    console.log("truffle shuffle", array);
-    return array;
-  }
 
 $(function() {
   var hiddenImg = "http://media-hearth.cursecdn.com/attachments/39/684/cardback_20.png";
@@ -28,25 +16,9 @@ $(function() {
       reveal($(this).attr("data-index"));
     });
     }
-
   var hearthCard = new hearthstoneCard();
   var cards = hearthCard.getImg();
-  var shuffledCards = shuffleArray(cards);
-  console.log("shuffled", shuffledCards);
-  // var cards;
-  // setTimeout(function() {
-  //   cards = hearthCard.cards;
-  // }, 1000)
-  //  = hearthCard.getImg();
-  console.log("not shuffled", cards);
 
-
-  console.log("things are looking up 2");
-  // console.log(hearthCard);
-  // $("#there").click(function() {
-  //   event.preventDefault();
-  //   console.log("clicked");
-  // });
 
   var correctGuesses = 0;
   var clicks = [];
@@ -63,11 +35,7 @@ $(function() {
         $("#card"+(index)).off("click");
         $("#card"+(clicks[0])).off("click");
         correctGuesses += 2;
-        // if (correctGuesses === cards.length) {
-        //   var won = setTimeout(function() {
-        //     alert("you WOn");
-        //   }, 100);
-        // }
+
         clicks = [];
       } else {
         $("#card"+(index)).html((cards[index]));
--- js/hearthstone.js
+++ js/hearthstone.js
@@ -4,6 +4,18 @@ function hearthstoneCard() {
  this.cards = [];
 }
 
+function shuffleArray(array) {
+  console.log(array.length);
+    for(var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        console.log("im inside shuffle function");
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    console.log("truffle shuffle1", array);
+    return array;
+  }
 
 
 
@@ -14,21 +26,20 @@ hearthstoneCard.prototype.getCards = function() {
 };
 
 hearthstoneCard.prototype.getImg = function() {
-  // var number = (Math.floor(Math.random() * 192)+1);
   var cards = [];
-  // var self = this;
   var shuffledCards;
   unirest.get("https://omgvamp-hearthstone-v1.p.mashape.com/cards/qualities/Epic")
 .header("X-Mashape-Key", "rWHfwQgzLMmshxTdrypY3GZNpimop1Wg51mjsnFotWj6HoCmAN")
 .header("Accept", "application/json")
 .end(function (result) {
-  // console.log(result.body[number], result.body[number].name);
-  // console.log(result);
   for (var i = 1; i <= 5; i++) {
     var number = (Math.floor(Math.random() * 192)+1);
     cards.push("<img src='"+result.body[number].imgGold+"'>");
     cards.push("<img src='"+result.body[number].imgGold+"'>");
   }
+
+  shuffledCards = shuffleArray(cards);
+  console.log(cards);
   //shuffle should run here. This is the function callback.
   //call frontend function to update UI from here?
 });
